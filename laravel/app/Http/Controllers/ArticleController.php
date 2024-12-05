6ff84bcc84a504ddcaa5a46d7a6c5c6e5b03cd7c
rahmadupi <yupiupiqwerty6@gmail.com>
2024-12-05 13:27:41 +0700

    remove duplicate import of Article model in ArticleController

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
-use App\Models\Article;
 
-class ArticleController extends Controller
 class ArticleController extends Controller
 {
     public function index()
